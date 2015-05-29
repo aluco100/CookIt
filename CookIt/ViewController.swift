7f5cb38f78b8b15da533c9bb35af20a5508b65df
Alfredo Luco <aluco@alumnos.uai.cl>
2015-05-29 10:07:54 -0400

    Initial Commit

---
+++ CookIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CookIt
+//
+//  Created by Alfredo Luco on 29-05-15.
+//  Copyright (c) 2015 Alfredo Luco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
